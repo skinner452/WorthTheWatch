521cad53e52ae540b9a6a7082a7c52dd4930989b
skinner452 <skinner452@gmail.com>
2015-03-30 13:51:34 -0500

    Finished average and predicted rating

---
+++ workspace/Testing/src/Testing.java
@@ -0,0 +1,9 @@
+
+public class Testing {
+    public static void main(String[] args) {
+    	String[] topCs = new String[3];
+        int[] topRs = new int[3];
+        System.out.println(topRs[0]);
+        System.out.println();
+	}
+}
